d3f2935d03c44d14c268983aa262deea4440e681
v8e0fs5bas69xu24OlRI3n <305807700+v8e0fs5bas69xu24OlRI3n@users.noreply.github.com>
2026-09-11 01:46:02 +0200

    Update here.js

--- docs/here.js
+++ docs/here.js
@@ -1,9 +1,9 @@
 const CONFIG = {
 
-    buyPrice: "$___",
+    buyPrice: "$39,750",
 
-    leasePrice: "$___",
+    leasePrice: "$1,825",
 
-    leaseText: "p.m. x ___",
+    leaseText: "p.m. x 24",
 
 };
